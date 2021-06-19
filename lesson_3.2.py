ed4031fab13a8cb248c69dc43f978ec133c419dd
a.shakh <vppasdapvv@gmail.com>
2021-06-19 12:23:20 +0300

    add lesson_3.py

---
+++ lesson_3.2.py
@@ -0,0 +1,29 @@
+# coding=utf-8
+# my_str = '0123456789'
+# print(len(my_str))
+#
+# number = 123356789098456789023456
+# print(len(str(number)))
+#
+#
+# index = -2
+# print(my_str[index])
+
+# срез
+
+# new_str = my_str[1:5]  # от(включая) до(не включая)
+# new_str = my_str[1:6:2]  # от:до:шаг
+# p_str = my_str[::2]  # четные места в строке
+# n_str = my_str[1::2]  # нечетные места в строке
+# back_str = my_str[::-1]  # разворот строки
+# print(p_str)
+# print(n_str)
+# print(back_str)
+
+name = 'John'
+age = 23
+
+hello_message = f'Знакомтесь, это {name}. Ему {age}'
+print(hello_message)
+
+
--- lesson_3.py
+++ lesson_3.py
@@ -1,1 +1,59 @@
-print('Hello')
+# coding=utf-8
+# if
+# if условие:
+#     блок, если да
+# else:
+#     блок, если нет
+#
+# value = 41
+#
+# if value == 4:
+#     print('!!!!')
+#     print(value * 3)
+#     new_value = 'asd' * value
+#     print(new_value)
+# elif value > 4:
+#     print('value > 4')
+# else:
+#     print('value < 4')
+# print('The end')
+
+
+# int to bool = true, кроме 0
+# float to bool = true, кроме 0.0
+# str to bool = true, крому ''
+
+# value1 = ''
+# my_value = bool(value1)
+# print(my_value)
+
+
+# my_bool = False
+# value = str(my_bool)
+# if value:
+#     print(value)
+# print('The end')
+
+# value = 3
+# if not value % 2 and not value % 3:
+#     print ('/2 and /3')
+# elif not value % 2:
+#     print('/2')
+# elif not value % 3:
+#     print('/3')
+# else:
+#     print('not /2 or /3')
+
+# my_str = 'qwerty'
+# my_int = 9
+# if 'r' in my_str:
+#     new_str = my_str * 2
+# else:
+#     new_str = my_str
+
+# тернарный оператор
+# new_str = my_str * 2 if 'r' in my_str else my_str
+# new_int = my_int / 2 if my_int > 10 else -1
+#
+# print(new_str)
+# print(new_int)
